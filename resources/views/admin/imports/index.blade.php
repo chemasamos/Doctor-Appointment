b8dfe66d5306ccc94dcf8cc8a2e50e6b6aa387af
Jose Samos <chemasassss@gmail.com>
2026-03-26 17:17:31 -0600

    fix: change extending layouts.admin to using x-admin-layout component in imports view

--- resources/views/admin/imports/index.blade.php
+++ resources/views/admin/imports/index.blade.php
@@ -1,27 +1,16 @@
-@extends('layouts.admin')
+<x-admin-layout
+    title="Importación Masiva de Pacientes"
+    :breadcrumbs="[
+        [
+            'name' => 'Dashboard',
+            'href' => route('admin.dashboard'),
+        ],
+        [
+            'name' => 'Importación',
+        ],
+    ]">
 
-@section('content')
-<div class="p-4 sm:ml-64">
-    <div class="p-4 rounded-lg mt-14">
-        <!-- Breadcrumbs -->
-        <nav class="flex mb-4" aria-label="Breadcrumb">
-            <ol class="inline-flex items-center space-x-1 md:space-x-3">
-                <li class="inline-flex items-center">
-                    <a href="{{ route('admin.dashboard') }}" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-                        <svg aria-hidden="true" class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
-                        Dashboard
-                    </a>
-                </li>
-                <li aria-current="page">
-                    <div class="flex items-center">
-                        <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                        <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Importación</span>
-                    </div>
-                </li>
-            </ol>
-        </nav>
-
-        <h2 class="mb-4 text-2xl font-bold text-gray-900 dark:text-white">Importación Masiva de Pacientes</h2>
+    <div class="mt-4">
 
         @if(session()->has('alert'))
             <div class="p-4 mb-4 text-sm text-{{ session('alert.type') == 'success' ? 'green' : 'red' }}-800 rounded-lg bg-{{ session('alert.type') == 'success' ? 'green' : 'red' }}-50 dark:bg-gray-800 dark:text-{{ session('alert.type') == 'success' ? 'green' : 'red' }}-400" role="alert">
@@ -86,5 +75,4 @@
             </div>
         </div>
     </div>
-</div>
-@endsection
+</x-admin-layout>
